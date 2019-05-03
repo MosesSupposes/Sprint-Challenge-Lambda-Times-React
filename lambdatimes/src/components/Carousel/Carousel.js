2c9ac9c0073dac44745a5b8b1dc44dc1b2f6dc89
Moses Samuel <mosamuel98@gmail.com>
2019-05-03 13:38:11 -0700

    stretch:  implement an infinite scroll carousel

--- lambdatimes/src/components/Carousel/Carousel.js
+++ lambdatimes/src/components/Carousel/Carousel.js
@@ -1,35 +1,62 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import { carouselData } from '../../data'
-// Complete this Carousel 
-export default class Carousel extends Component {
-  constructor(props){
-    super(props);
-    this.state = {
 
+
+export default function Carousel(props) {
+  const [images, setImages] = useState(carouselData)
+  const [activeDisplay, setActiveDisplay] = useState(images[0])
+  const [activeIndex, setActiveIndex] = useState(0)
+
+
+
+  const handleClick = (direction) => {
+    const firstImg = images[0]
+    const lastImg = images[images.length - 1]
+
+    switch(direction) {
+      case 'left': 
+        if (activeIndex === 0) {
+          setActiveDisplay(lastImg)
+          setActiveIndex(images.indexOf(activeDisplay))
+        } else {
+          setActiveDisplay(images[activeIndex - 1]) 
+          setActiveIndex(activeIndex - 1)
+        }
+        break
+      case 'right': 
+        if (activeIndex >= images.length - 1) {
+          setActiveDisplay(firstImg)
+          setActiveIndex(0)
+        } else {
+          setActiveDisplay(images[activeIndex + 1])
+          setActiveIndex(images.indexOf(activeDisplay))
+        }
+        break
     }
   }
-  componentDidMount(){
 
-  }
-
-  leftClick = () => {
-
-  }
-
-  rightClick = () => {
-
-  }
-
-  selectedImage = () => {
-    return <img src={} style={{display: 'block'}} />
-  }
+  // const selectedImage = () => {
+  //   return <img src={''} style={{display: 'block'}} />
+  // }
   
-  render(){
-    return (
-      <div className="carousel">
-        <div className="left-button" onClick={this.leftClick}>{"<"}</div>
-        <div className="right-button" onClick={this.rightClick}>{">"}</div>
+
+  return (
+    <div className="carousel">
+      <div 
+        className="left-button" 
+        onClick={handleClick.bind(null, 'left')}
+      >
+          {"<"}
       </div>
-    )
-  }
+      
+      <img style={{display:'block'}}src={activeDisplay}></img>
+
+      <div 
+        className="right-button" 
+        onClick={handleClick.bind(null, 'right')}
+      >
+          {">"}
+      </div>
+    </div>
+  )
 }
